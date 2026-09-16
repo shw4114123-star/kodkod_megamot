4648397be3e0f6eb9ab6edf727993d64c804d528
שלמה וקסמן <shw4114123@gmail.com>
2026-09-16 11:26:38 +0300

    added a litlle change in the verifyToken function to throw a specific error

--- week23/authenticationFlow/server/services/generateToken.js
+++ week23/authenticationFlow/server/services/generateToken.js
@@ -1,11 +1,16 @@
 import "dotenv/config"
 import jwt from "jsonwebtoken"
+import { craeteError } from "./errorHandler.js";
 
-export function generateToken(userId){
-    return jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES_IN})
+export function generateToken(userId) {
+    return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN })
 }
 
 
 export function verifyToken(token) {
-    return jwt.verify(token, process.env.JWT_SECRET);
+    try {
+        return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+        throw new craeteError("Unauthorized", 401)
+    }
 }
